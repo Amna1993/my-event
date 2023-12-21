ea726e43db7d29a53aa42fd384fbb8769c6904db
Amna1993 <amnarazzaqawan@gmail.com>
2023-12-21 04:58:14 -0500

    Buffer

--- src/components/Update.js
+++ src/components/Update.js
@@ -1,19 +1,14 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-const Update = () => {
-  const [postId, setPostId] = useState('');
-  const [updatedTitle, setUpdatedTitle] = useState('');
+const Create = () => {
+  const [newPostTitle, setNewPostTitle] = useState('');
 
-  const handlePostIdChange = (event) => {
-    setPostId(event.target.value);
+  const handleInputChange = (event) => {
+    setNewPostTitle(event.target.value);
   };
 
-  const handleUpdatedTitleChange = (event) => {
-    setUpdatedTitle(event.target.value);
-  };
-
-  const handleUpdatePost = async () => {
+  const handleCreatePost = async () => {
     try {
       // Fetch the current content of db.json from GitHub using the GitHub API
       const response = await axios.get(
@@ -22,70 +17,54 @@ const Update = () => {
 
       // Parse the content of the file
       const currentData = JSON.parse(
-        Buffer.from(response.data.content, 'base64').toString('utf-8')
+        atob(response.data.content.replace(/\s/g, ''))
       );
 
-      // Find the index of the post to update
-      const indexToUpdate = currentData.findIndex(
-        (post) => post.id === parseInt(postId)
-      );
+      // Add a new post
+      const newPost = {
+        id: currentData.length + 1,
+        title: newPostTitle,
+      };
 
-      // If the post exists, update its title
-      if (indexToUpdate !== -1) {
-        currentData[indexToUpdate].title = updatedTitle;
+      currentData.push(newPost);
 
-        // Update the content of db.json on GitHub
-        await axios.put(
-          'https://api.github.com/repos/Amna1993/my-event/contents/db.json',
-          {
-            message: 'Update post',
-            content: Buffer.from(JSON.stringify(currentData)).toString(
-              'base64'
-            ),
-            sha: response.data.sha,
+      // Update the content of db.json on GitHub
+      await axios.put(
+        'https://api.github.com/repos/Amna1993/my-event/contents/db.json',
+        {
+          message: 'Create post',
+          content: btoa(JSON.stringify(currentData)),
+          sha: response.data.sha,
+        },
+        {
+          headers: {
+            Authorization: `Bearer YOUR_GITHUB_TOKEN`, // Replace with your GitHub token
           },
-          {
-            headers: {
-              Authorization: `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`, // Replace with your GitHub token
-            },
-          }
-        );
+        }
+      );
 
-        // Clear the input fields after updating a post
-        setPostId('');
-        setUpdatedTitle('');
-      } else {
-        console.error('Post not found');
-      }
+      // Clear the input field after creating a post
+      setNewPostTitle('');
     } catch (error) {
-      console.error('Error updating post:', error);
+      console.error('Error creating post:', error);
     }
   };
 
   return (
     <div>
-      <h2>Update a Post</h2>
+      <h2>Create a New Post</h2>
       <div>
-        <label htmlFor="postId">Post ID:</label>
+        <label htmlFor="newPostTitle">Title:</label>
         <input
           type="text"
-          id="postId"
-          value={postId}
-          onChange={handlePostIdChange}
+          id="newPostTitle"
+          value={newPostTitle}
+          onChange={handleInputChange}
         />
+        <button onClick={handleCreatePost}>Create Post</button>
       </div>
-      <div>
-        <label htmlFor="updatedTitle">Updated Title:</label>
-        <input
-          type="text"
-          id="updatedTitle"
-          value={updatedTitle}
-          onChange={handleUpdatedTitleChange}
-        />
-      </div>
-      <button onClick={handleUpdatePost}>Update Post</button>
     </div>
   );
 };
 
-export default Update;
+export default Create;
